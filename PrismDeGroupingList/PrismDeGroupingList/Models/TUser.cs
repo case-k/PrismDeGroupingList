4bd9663935892a735f172f6f69f92d27662a24f5
Keisuke Ishikawa <ishikawa-k@mis-kyushu.jp>
2016-12-12 13:12:13 +0900

    二種類のリスト切り替えられるようにしてみた

---
+++ PrismDeGroupingList/PrismDeGroupingList/Models/TUser.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrismDeGroupingList.Models
+{
+    public class TUser
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Kana { get; set; }
+        public string Address { get; set; }
+        public DateTime Birthday { get; set; }
+
+        public int Age
+        {
+            get { return (DateTime.Today.Year - Birthday.Year) - (Birthday.DayOfYear > DateTime.Today.DayOfYear ? 1 : 0); }
+        }
+
+
+    }
+}
--- PrismDeGroupingList/PrismDeGroupingList/ViewModels/MainPageViewModel.cs
+++ PrismDeGroupingList/PrismDeGroupingList/ViewModels/MainPageViewModel.cs
@@ -1,35 +1,78 @@
 ﻿using Prism.Commands;
 using Prism.Mvvm;
 using Prism.Navigation;
+using PrismDeGroupingList.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 
 namespace PrismDeGroupingList.ViewModels
 {
-    public class MainPageViewModel : BindableBase, INavigationAware
+    public class MainPageViewModel : BindableBase
     {
-        private string _title;
-        public string Title
+        public List<TUser> Users { get; } = new List<TUser>
         {
-            get { return _title; }
-            set { SetProperty(ref _title, value); }
+            new TUser
+            {
+                ID = 0, Name = "あ", Kana = "ア", Address = "大分市",
+                Birthday = DateTime.Parse("1974/01/01")
+            },
+            new TUser
+            {
+                ID = 0, Name = "い", Kana = "イ", Address = "別府市",
+                Birthday = DateTime.Parse("1974/02/02")
+            },
+            new TUser
+            {
+                ID = 0, Name = "う", Kana = "ウ", Address = "中津市",
+                Birthday = DateTime.Parse("1975/01/01")
+            },
+            new TUser
+            {
+                ID = 0, Name = "え", Kana = "エ", Address = "別府市",
+                Birthday = DateTime.Parse("1976/01/01")
+            },
+            new TUser
+            {
+                ID = 0, Name = "お", Kana = "オ", Address = "日田市",
+                Birthday = DateTime.Parse("1975/03/03")
+            },
+            new TUser
+            {
+                ID = 0, Name = "か", Kana = "カ", Address = "大分市",
+                Birthday = DateTime.Parse("1973/05/05")
+            },
+            new TUser
+            {
+                ID = 0, Name = "き", Kana = "キ", Address = "大分市",
+                Birthday = DateTime.Parse("1977/06/06")
+            },
+        };
+
+
+        private bool _dataType;
+        public bool DisplayType
+        {
+            get { return _dataType; }
+            set { SetProperty(ref _dataType, value); }
         }
+
+
+        public List<IGrouping<string, TUser>> GroupingUsersByAddress
+        {
+            get { return Users.GroupBy(user => user.Address).ToList(); }
+        }
+
+        public List<IGrouping<int, TUser>> GroupingUsersByAge
+        {
+            get { return Users.GroupBy(user => user.Age).ToList(); }
+        }
+
 
         public MainPageViewModel()
         {
-
+            DisplayType = false;
         }
 
-        public void OnNavigatedFrom(NavigationParameters parameters)
-        {
-
-        }
-
-        public void OnNavigatedTo(NavigationParameters parameters)
-        {
-            if (parameters.ContainsKey("title"))
-                Title = (string)parameters["title"] + " and Prism";
-        }
     }
 }
