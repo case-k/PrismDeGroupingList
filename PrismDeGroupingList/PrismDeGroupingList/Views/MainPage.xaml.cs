0452bc299ad34149d6569a15130a0264ec040548
Keisuke Ishikawa <ishikawa-k@mis-kyushu.jp>
2016-12-12 10:15:50 +0900

    プロジェクト ファイルを追加します。

---
+++ PrismDeGroupingList/PrismDeGroupingList/Views/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace PrismDeGroupingList.Views
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
